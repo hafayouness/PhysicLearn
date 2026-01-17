f263018e0bb2ab2e569762e2791b62da03fbdf84
hafayouness <155436001+hafayouness@users.noreply.github.com>
2026-01-17 21:59:39 +0100

    Gestion des Quiz

--- backend/Models/index.js
+++ backend/Models/index.js
@@ -1,7 +1,7 @@
 import User from "./User.js";
 import Course from "./Course.js";
 import Lesson from "./Lesson.js";
-
+import Quiz from "./Quiz.js";
 Course.hasMany(Lesson, {
   foreignKey: "id_course",
   as: "lessons",
@@ -12,4 +12,14 @@ Lesson.belongsTo(Course, {
   as: "course",
 });
 
-export { User, Course, Lesson };
+Course.hasMany(Quiz, {
+  foreignKey: "id_course",
+  as: "quizzes",
+  onDelete: "CASCADE",
+});
+Quiz.belongsTo(Course, {
+  foreignKey: "id_course",
+  as: "course",
+});
+
+export { User, Course, Lesson, Quiz };
